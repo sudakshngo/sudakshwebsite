d0aefc3059b5a1a93194d0360fa901b4a7247015
Abhishek Kansal <abhishekkansal73@gmail.com>
2020-06-12 17:38:05 +0530

    final website

---
+++ js/scroll.js
@@ -0,0 +1,42 @@
+
+function about(){
+    let elmnt = document.getElementById("about");
+    elmnt.scrollIntoView({
+      behavior:"smooth"
+    });
+  }
+
+function project(){
+  let elmnt = document.getElementById("project");
+  elmnt.scrollIntoView({
+    behavior:"smooth"
+  });
+}
+
+function testimonial(){
+  let elmnt = document.getElementById("testimonial");
+elmnt.scrollIntoView({
+  behavior:"smooth"
+});
+}
+
+function donate(){
+  let elmnt = document.getElementById("donate");
+  elmnt.scrollIntoView({
+    behavior:"smooth"
+  });
+}
+
+function contact(){
+  let elmnt = document.getElementById("contact");
+  elmnt.scrollIntoView({
+    behavior:"smooth"
+  });
+}
+
+function joinUs(){
+  let elmnt = document.getElementById("joinUs");
+  elmnt.scrollIntoView({
+    behavior:"smooth"
+  });
+}
